6e062d824624610d666862aa6715dc4b188342ee
casares lucas <casares.lucasgaston@iesconcepcion.edu.ar>
2025-02-14 20:59:27 -0300

    agregando componente

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,46 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Products } from './components/Products'
+import { Carrito } from './components/Carrito'
 
-function App()  {
+function App() {
   const [products, setProducts] = useState([])
-const [shop,setShop] = useState([])
+  const [shop, setShop] = useState([])
+  const [shopPage, setShopPage] = useState(false)
 
-const getProducts = async ()=>{
-let response =await fetch("https://fakestoreapi.com/products")
-let data = await response.json()
-setProducts(data)
-}
+  const getProducts = async () => {
+    let response = await fetch("https://fakestoreapi.com/products")
+    let data = await response.json()
+    setProducts(data)
+  }
 
-useEffect(()=>{
-  getProducts()
-},[])
+  useEffect(() => {
+    getProducts()
+  }, [])
 
-const buyProduct = (idProduct)=>{
-  setShop([...shop,idProduct])
-}
+  const buyProduct = (idProduct) => {
+    setShop([...shop, idProduct])
+  }
 
-if(products.length== 0){
-return<>
-<h1>Cargando...</h1>
-</>
-}
+  if (products.length == 0) {
+    return <>
+      <h1>Cargando...</h1>
+    </>
+  }
+  if (shopPage == false) {
+    return (
+      <>
+        <h1>Fake Store</h1>
+        <button disabled={shop.length == 0} onClick={() => {setShopPage(true)}}>Carrito {shop.length}</button >
 
-return (
-<>
-<h1>Fake Store</h1>
-
-<Products products={products} buyProduct={buyProduct}/>
+          <Products products={products} buyProduct={buyProduct} />
 </>
 )
+  } else {
+    return <>
+      <Carrito shop={shop} setShopPage={setShopPage} setShop={setShop} />
+    </>
+  }
 }
 
 export default App
---
+++ src/components/Carrito.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+export const Carrito = ({ shop, setShopPage,setShop })=>{
+    const[total, setTotal] =useState(0)
+
+    useEffect (()=>{
+    setTotal(shop.reduce((suma, product)=> suma+product.price,0))
+    },[shop])
+
+    return <>
+        < h1 > Fake Store</h1 >
+    <button onClick ={()=>{ setShopPage(false)}}>Volver</button>
+        <table>
+            <thead>
+                <tr>
+                    <th>Producto</th>
+                    <th>Precio</th>
+                </tr>
+            </thead>
+            <tbody>
+                {shop.map((element, index) => {
+                    return <tr key={index}>
+                        <td>{element.title}</td>
+                        <td>{element.price}</td>
+
+                    </tr>
+                })}
+
+            </tbody>
+        </table>
+        <h2>Total: {total}</h2>
+<button onClick={()=>{
+    setShopPage(false)
+    setShop([])
+    alert("Gacias por su compra")
+}}> Realizar Compra</button>
+    </>
+
+
+
+
+
+}
